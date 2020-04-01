5a59c99c9352b123017b20f134be5940f8b34db1
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-04-01 17:09:59 +1100

    Changed to have dotenv example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [imgSrc, setImgSrc] = useState("");
+
+  // Set our environment to an easier to use name
+  const environment = process.env.NODE_ENV;
+
+  // Set our secret to the environment variable
+  const secret = process.env.REACT_APP_SECRET;
+
+  // We should be using this in a production environment
+  const getDoggos = () => {
+    console.log("Getting a doggo");
+    console.log(`Shhhhh, our secret is:\n ${secret}`);
+    setImgSrc("https://placedog.net/600/400");
+  };
+
+  // We should use this in a development environment
+  const getKitties = () => {
+    console.log("Getting a kitten");
+    console.log(`Shhhhh, our secret is:\n ${secret}`);
+    setImgSrc("https://placekitten.com/600/400");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* We can use the environment to change the behaviour of the app */}
+      {environment == "production" ? (
+        <div>
+          <p>Doggo Viewer</p>
+          <button onClick={getDoggos}>Click to see a doggo</button>
+        </div>
+      ) : (
+        <div>
+          <p>Kitty Viewer</p>
+          <button onClick={getKitties}>Click to see a kitty</button>
+        </div>
+      )}
+      <img src={imgSrc} />
     </div>
   );
-}
+};
 
 export default App;
